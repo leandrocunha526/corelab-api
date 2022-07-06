12fe619b441ef8a1c282dc0a91824be738491ff7
Leandro Cunha <leandrocunha016@gmail.com>
2022-07-06 19:09:18 -0300

    Add delete and update

--- src/controllers/vehicles.controller.ts
+++ src/controllers/vehicles.controller.ts
@@ -3,14 +3,19 @@ import { Router } from "express"
 const router = Router()
 
 //Services
-import { CreateVehicle, GetAllVehicles } from "../services/vehicles.service"
+import {
+    CreateVehicle,
+    GetAllVehicles,
+    DeleteVehicle,
+    UpdateVehicle
+} from "../services/vehicles.service"
 
 router.post('/register', async(req, res) => {
     try{
         const vehicle = await CreateVehicle(req.body)
         res.status(201).json(vehicle)
     } catch(error){
-        res.status(500).json(error)
+        res.status(500).json({message: "Error 500: Internal server error"})
     }
 })
 
@@ -19,7 +24,34 @@ router.get('/list', async(req, res) => {
       const vehiclesList = await GetAllVehicles()
       res.status(200).json(vehiclesList)
     } catch(error){
-        res.status(500).json(error)
+        res.status(500).json({message: "Error 500: Internal server error"})
+    }
+})
+
+router.put('/update', async(req, res) => {
+    try {
+        const vehicle = await UpdateVehicle(req.body)
+        res.status(200).json(vehicle)
+    } catch(error){
+        if(error === 'vehicle_not_found'){
+            res.status(404).send({message: "Error 404: Vehicle not found"})
+        } else{
+            res.status(500).json({message: "Error 500: Internal server error"})
+        }
+    }
+})
+
+router.delete("/delete/:id?", async(req, res) => {
+    try {
+        const vehicle = await DeleteVehicle(req.params.id)
+        res.status(200).send({message: vehicle.id + "it was excluded"})
+    }catch(error){
+        if(error === 'vehicle_not_found') {
+            res.status(404).json(error)
+        }
+        else{
+            res.status(500).json({message: "Error 500: Internal server error"})
+        }
     }
 })
 
--- src/services/vehicles.service.ts
+++ src/services/vehicles.service.ts
@@ -17,3 +17,33 @@ export const GetAllVehicles = async() => {
         throw error
     }
 }
+
+export const DeleteVehicle = async(data: any) => {
+    try{
+        const vehicle = await Vehicles.findByIdAndDelete(data)
+
+        if(vehicle === null){
+            throw new Error('vehicle_not_found')
+        }
+
+        return vehicle
+    }catch(error){
+        throw error
+    }
+}
+
+export const UpdateVehicle = async(data: any) => {
+    try{
+        const vehicle = await Vehicles.findByIdAndUpdate({
+          _id: data._id,
+        }, data, {new: true})
+
+        if(vehicle === null){
+            throw new Error('vehicle_not_found')
+        }
+
+        return vehicle
+    }catch(error){
+        throw error
+    }
+}

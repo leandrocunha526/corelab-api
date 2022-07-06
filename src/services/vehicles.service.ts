ac751a1778e2cd40ff2223f45dfa453af4958d9d
Leandro Cunha <leandrocunha016@gmail.com>
2022-07-05 22:52:31 -0300

    Add get all vehicle

--- src/services/vehicles.service.ts
+++ src/services/vehicles.service.ts
@@ -4,7 +4,18 @@ export const CreateVehicle = async(data: any) => {
     try {
         const vehicle = await Vehicles.create(data)
         return vehicle
-    } catch(err){
-        throw err
+    } catch(error){
+        throw error
+    }
+}
+
+export const GetAllVehicle = async() => {
+    try{
+      const vehicle = await Vehicles.find()
+      if(vehicle == null){
+        throw new Error("No vehicle found")
+      }
+    } catch(error){
+        throw error
     }
 }

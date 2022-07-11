a3a833ef0ae1af6d395e8439aca4a866cef9cbe9
Leandro Cunha <leandrocunha016@gmail.com>
2022-07-11 17:57:10 -0300

    Import filters

--- src/controllers/vehicles.controller.ts
+++ src/controllers/vehicles.controller.ts
@@ -8,7 +8,9 @@ import {
     GetAllVehicles,
     DeleteVehicle,
     UpdateVehicle,
-    Favorite
+    Favorite,
+    GetVehiclesForFilters,
+    GetVehiclesByFilters
 } from "../services/vehicles.service"
 
 router.post('/register', async(req, res) => {
@@ -62,6 +64,27 @@ router.post('/favorite', async(req, res) => {
         res.status(200).json(favorite)
     } catch(error){
         res.status(500).json({message: "Error 500: Internal server error"})
+    }
+})
+
+router.get("/filters", async(req, res) => {
+    try{
+        const data = await GetVehiclesForFilters()
+        res.status(200).json(data)
+    }catch(error){
+        res.status(500).json(error)
+    }
+})
+
+router.get("/filtered", async (req, res) => {
+    try{
+        const data = await GetVehiclesByFilters(req.query)
+        res.status(200).json(data)
+    }catch(error){
+        if(error.message === "vehicle_not_found"){
+            res.status(404).json({message: error.message})
+        }
+        res.status(500).json(error)
     }
 })
 

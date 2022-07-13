5839324c7b62d5dece652f2d7b68b92d37e79f43
Leandro Cunha <leandrocunha016@gmail.com>
2022-07-13 16:07:40 -0300

    Add filters

--- src/services/vehicles.service.ts
+++ src/services/vehicles.service.ts
@@ -41,7 +41,6 @@ export const UpdateVehicle = async(data: any) => {
         if(vehicle === null){
             throw new Error('vehicle_not_found')
         }
-
         return vehicle
     }catch(error){
         throw error
@@ -56,5 +55,66 @@ export const Favorite = async(data: any) => {
          return favorite
     } catch(error){
         throw error
+    }
+}
+
+export const GetVehiclesForFilters = async () => {
+    try{
+        const VehiclesData = {
+            name: await Vehicles.distinct("name"),
+            year: await Vehicles.distinct("year"),
+            color: await Vehicles.distinct("year")
+        }
+        return VehiclesData
+    } catch(error){
+        throw error
+    }
+}
+
+export const GetVehiclesByFilters = async (data: any) => {
+    try{
+        let dataVehicles = []
+        const search = []
+        const filter = JSON.parse(data.filters)
+
+        if(filter.price !== undefined){
+            filter.price["$lte"] = filter.price["priceMax"]
+            filter.price["$gte"] = filter.price["priceMin"]
+            delete filter.price['priceMax']
+            delete filter.price['priceMin']
+
+            Object.entries(filter.price).forEach(([key, value]) => {
+                if(value === undefined || value === "")
+                    delete filter.price[key]
+            })
+            if(JSON.stringify(filter.price) === '{}'){
+                delete filter['price']
+            }
+        }
+        Object.entries(filter).forEach(([key, value]) => {
+            if(value !== undefined && value !== 'Todos'){
+                search.push({[key]: value})
+            }
+        })
+        dataVehicles = search.length > 0 ? await Vehicles.find({$and: search}) :  await Vehicles.find()
+
+        let vehiclesData = []
+
+        vehiclesData = dataVehicles.filter(Vehicle =>
+            Object.entries(Vehicle._doc).find(([key, value]) => {
+                if(value.toString().toLowerCase().includes(data.keyword.toLowerCase()) || data.keyword === ""){
+                    return true
+                }
+            })
+        )
+
+        if(dataVehicles.length === 0){
+            throw new Error("vehicle_not_found")
+        }
+
+        return vehiclesData
+
+    } catch(error){
+        throw Error
     }
 }
